b961a712ecbc8fe8fb2867bf4f93bb13d7270de6
Imran Azeezullah <imran@kanopicover.com>
2022-12-12 13:12:06 +1000

    Add logging on test api call

--- tests/placeholder/todos.py
+++ tests/placeholder/todos.py
@@ -3,7 +3,10 @@ from baseapi.apis import RestApi
 
 class TodosApi(RestApi):
     def list_todos(self):
-        return self.get('/todos')
+        # The following logging will not show up on pytest without extra flags:
+        # pytest -o log_cli=true -o log_cli_level=info
+        self.client.logger.info("Retrieving todos...")
+        return self.get("/todos")
 
     def _just_a_test(self):
         # Shouldn't be exposed.
